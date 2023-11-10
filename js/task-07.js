f5b6a194664ea00a936c50597405977ac27c2274
Yuliia <djudit@ukr.net>
2023-11-11 01:58:40 +0200

    started homework

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputRange = document.querySelector("#font-size-control");
-const spanText = document.querySelector("#text");
-
-inputRange.addEventListener("input", handleRange);
-
-function handleRange(event) {
-  spanText.style.fontSize = inputRange.value + "px";
-}
