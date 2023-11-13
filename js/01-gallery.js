9fb83a8c4fb6e7d306f97a342091b5697b6c104f
Yuliia <djudit@ukr.net>
2023-11-13 02:48:53 +0200

    task 1 - gallery was added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function createGallery(pictures) {
+  return pictures
+    .map(
+      ({ original, preview, description }) => `
+      <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+const itemPicture = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", itemPicture);
+
+//   Copied from basicLightbox
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+
+instance.show();
