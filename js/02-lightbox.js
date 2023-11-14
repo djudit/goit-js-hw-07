60e48e36aad0d45c5c8f0033c82e0b1a965b307e
Yuliia <djudit@ukr.net>
2023-11-14 17:19:01 +0200

    Task 2 finished

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,46 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-import * as basicLightbox from "basiclightbox";
+const gallery = document.querySelector(".gallery");
 
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
+gallery.addEventListener("click", handlerClick);
 
-instance.show();
+function createGallery(pictures) {
+  return pictures
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+      `;
+    })
+    .join("");
+}
 
-console.log(galleryItems);
+const itemPicture = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", itemPicture);
+
+function handlerClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let gallerySimple = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  gallerySimple.on("show.simplelightbox");
+}
+
+// import * as basicLightbox from "basiclightbox";
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
+
+// console.log(galleryItems);
